a49322db071d64a45b8ac4aa736c312f7682c30a
Michelle Fullwood <michelle.fullwood@gmail.com>
2015-10-04 23:58:37 -0400

    Removed tags functionality from filter.py, now all in tags.py

--- ao3/filter.py
+++ ao3/filter.py
@@ -41,11 +41,6 @@ for fic_id, fic in fics.items():
     if age < MINIMUM_AGE:
         continue
 
-    # remove fics that contain 1 or more unwanted tags (specified in OMIT_TAGS)
-    if ('freeform' in fic.keys() and
-            set(OMIT_TAGS).intersection(set(fic['freeform']))):
-        continue
-
     # if it's passed all the filters
     filtered[fic_id] = fic
 
